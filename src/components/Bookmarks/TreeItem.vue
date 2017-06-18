f25b93e50b6bee031bcdca51a14dd31fa87bde3f
helloyoucan <helloyoucan@163.com>
2017-06-18 23:19:34 +0800

    edit bookmarks tree

---
+++ src/components/Bookmarks/TreeItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+  <span>{{node.data.label}}</span>
+    <span>
+      <el-button size="mini">添加</el-button>
+       <el-button size="mini">删除</el-button>
+    </span>
+  </span>
+</template>
+<script>
+
+  export default {
+    name: '',
+    components: {},
+    props: ['node'],
+    data() {
+      return {}
+    },
+    methods: {},
+    created(){
+    },
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
